79fed7fff7b2fb959aad7a90544e806c32810a38
Matt Radford <quinquex@gmail.com>
2025-03-20 12:55:59 -0700

    org chart

--- src/components/OrgChart/OrgChart.jsx
+++ src/components/OrgChart/OrgChart.jsx
@@ -4,36 +4,28 @@ const OrgChart = () => {
     return (
         <main>
             <h1>Org Chart</h1>
+            <br />
             <div className="org">
-                <br />
-                Event Horizon Liability Solutions
-                <hr />
+                <div>Event Horizon Liability Solutions</div>
                 <div>
                     <div>
-                        <div>|</div>
-                        C-Suite
-                        <div>|</div>
+                        <header className="org-group">C-Suite</header>
                         <div>Celestial Risk Assessor</div>
                         <div>Void Contract Analyst</div>
                     </div>
                     <div>
-                        <div>|</div>
-                        Field Agents
-                        <div>|</div>
+                        <header className="org-group">Field Agents</header>
                         <div>GravWell Actuary</div>
                         <div>Disaster Response Negotiator</div>
                     </div>
                     <div>
-                        <div>|</div>
-                        Security
-                        <div>|</div>
+                        <header className="org-group">Security</header>
                         <div>Quantum Risk Mitigation Unit</div>
                         <div>Asset Recovery Specialist</div>
                     </div>
                 </div>
             </div>
         </main>
-
     );
 };
 
