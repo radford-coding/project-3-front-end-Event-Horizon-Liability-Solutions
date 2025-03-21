41fab9d29e78a1b52c0cfa04825fd1cbcb257273
2000Bel <belvelizm@gmail.com>
2025-03-21 18:35:28 -0400

    sort in EmployeeList.jsx typewriter in EmployeeList.css

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -13,20 +13,27 @@ const EmployeeList = () => {
         const fetchEmployees = async () => {
             const fetchedEmployees = await userService.employeeList(user._id);
             console.log(user);
-            setEmployees(fetchedEmployees);
+             //sort EmployeeList alphabetically first name 
+             const sortedEmployees = fetchedEmployees.sort((a, b) => 
+                a.fullname.localeCompare(b.fullname)
+            );
+            setEmployees(sortedEmployees);
         };
         fetchEmployees();
     }, [user]);
+    
 
     return (
         <main>
             <h2>EHLS Employees</h2>
             <ul>
+            <div className="typewriter">
                 {employees.map((employee) => (
                     <NavLink key={employee._id} to={`/employees/${employee._id}`}>
                         <li>{employee.fullname}</li>
                     </NavLink>
                 ))}
+                </div>
             </ul>
             <NavLink to='/orgchart/'><button type='button'>Org Chart</button></NavLink>
         </main>
