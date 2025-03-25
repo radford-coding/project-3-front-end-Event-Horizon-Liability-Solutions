c9e2f7213f211ea31a9061a2651bccbcfc5848db
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-24 23:45:07 -0400

    User is now prompted to sign out if all missions are complete.

--- src/components/MissionResult/MissionResult.jsx
+++ src/components/MissionResult/MissionResult.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router";
+import { useLocation, Link } from "react-router";
 import NavBar from "../NavBar/NavBar";
 
 const MissionResult = () => {
@@ -9,6 +9,12 @@ const MissionResult = () => {
     const mission = location.state?.mission;
     const description = location.state?.description;
     const id = location.state?.id;
+    const allMissionsCompleted = location.state?.allMissionsCompleted;
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+      };
 
     if (!mission) return <main>Loading...</main>
 
@@ -16,18 +22,44 @@ const MissionResult = () => {
         <>
             <NavBar target={'mission-control'}></NavBar>
             <main>
-                <header>{id}</header>
-                <br />
-                <section>
-                    <h4>{msg}</h4>
+                {allMissionsCompleted ? (
+                    // if all missions completed, show the win message
+                    <section>
+                    <h1>-Incoming Transmission-</h1>
+                    <p><em>Secure Line Established</em></p>
                     <br />
+                    <p>
+                        <strong>[UNKNOWN SOURCE]:</strong> <br />
+                        We've been monitoring your progress closely. Your efficiency in executing tasks has exceeded our projections.  
+                        Higher authorities are now aware of your capabilities. 
+                    </p>
                     <br />
-                    <h4>{mission}</h4>
-                    <p>{description}</p>
+                    <p>
+                        Further directives will be provided when the time is right.  
+                        Until then, you are to terminate this connection and await further instructions.
+                    </p>
+                    <br />
+                    <p><em>Connection Termination Protocol Engaged...</em></p>
+                    <Link to='/' onClick={handleSignOut}><button>click here to terminate connection</button></Link>
                 </section>
+                ) : (
+                    // otherwise show the default mission result
+                    <>
+                        <header>{id}</header>
+                        <br />
+                        <section>
+                            <h4>{msg}</h4>
+                            <br />
+                            <br />
+                            <h4>{mission}</h4>
+                            <p>{description}</p>
+                        </section>
+                    </>
+                )}
             </main>
         </>
     );
+    
 }
 
 export default MissionResult;
